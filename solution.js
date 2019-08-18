69e6873a4d35e1346a23397b85c5ad8b98b8fdd0
eadehemingway <eade.hemingway@gmail.com>
2019-08-18 12:26:47 +0300

    use prevState

--- solution.js
+++ solution.js
@@ -1,15 +1,14 @@
 class App extends React.Component{
-
     state = {
         number: 0
     }
 
     increment = () => {
-        this.setState({number: number +1})
+        this.setState((prevState)=> ({number: prevState.number +1}))
     }
 
     decrement = () => {
-        this.setState({number: number -1})
+        this.setState((prevState)=> ({number: prevState.number -1}))
     }
     render (){
         const {number} = this.state
@@ -18,7 +17,6 @@ class App extends React.Component{
                 < button onClick={this.increment}> add one </button>
                 < button onClick={this.decrement}> minus one </button>
                 <p> {number}</p>
-
                 {number === 10 && (<p>TEN!</p>)}
             </div>  
 
